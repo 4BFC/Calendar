1ce3672e81d323af6e48bea6c2a619a240517e42
4BFC <4bee.code@gmail.com>
2024-02-19 23:35:02 +0900

    feat: Attempt to connect to openweathermap API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import './styled.css';
 
-
+//api생성하고 컴포넌트 나누기
 function App() {
 
-  const [month, setMonth] = useState(); 
-  const [nameMonth, setNameMonth] = useState();
+  const [month, setMonth] = useState(moment().month() + 1);
+  const [nameMonth, setNameMonth] = useState(`(${moment().format('MMMM')})`);
 
   // '+' 버튼이 보여질지 여부를 결정하는 state
   const [buttonVisible, setButtonVisible] = useState({});
@@ -15,7 +15,7 @@ function App() {
   // input 안에 텍스트를 위한 state
   const [inputTexts, setInputTexts] = useState({});
 
-  
+
 
   const allDays = moment.weekdays();
   const allMonth = moment.months();
@@ -24,11 +24,31 @@ function App() {
   const first_WeekStartDate = spDate.clone().startOf('month').startOf('week');
   const last_WeekEndDate = spDate.clone().endOf('month').endOf('week');
 
-
   const weekDatesRow = [];
 
+  const location = 'Seoul';
+  // 날씨 API 열결
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=bc4e65baf81a66e7314e7db6dc80a301`);
+        const data = await res.json();
+        console.log(data.name);
+        console.log(data.weather[0].main);
+        console.log(data.main.temp_max - 273.15);
+        console.log(data.main.temp_min - 273.15);
+        console.log(data.weather[0].icon);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    // 주 단위 두 번째 for문
+    fetchData();
+  }, []);
+
+
+
+  // 주 단위 두 번째 for문
   for (let weekStart = first_WeekStartDate.clone(); weekStart.isBefore(last_WeekEndDate); weekStart.add(1, 'week')) {
     const weekEnd = weekStart.clone().endOf('week');
     const weekDates = []; // 각 주 마다의 일자 저장 배열
@@ -54,6 +74,7 @@ function App() {
         }));
       };
 
+
       // '+' 버튼을 클릭했을 때는 해당 날짜의 input 박스 배열에 새로운 요소를 추가한다.
       const handleClick = () => {
         setInputBoxes(prevState => ({ // InputBoxes 업데이트
@@ -70,40 +91,36 @@ function App() {
       // td태그 형태로  weekDates에 push
       weekDates.push(
         <td key={dayFormat} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {day.format('DD')}
-      {buttonVisible[dayFormat] && <button onClick={handleClick}>+</button>}
-      <br/>
-      {/* input 박스의 onChange 이벤트를 처리하여 입력된 텍스트를 state에 저장합니다. */}
-      {(inputBoxes[dayFormat] || []).map((_, index) => (
-        <div>
-          <input
-            key={index}
-            type="text"
-            value={inputTexts[dayFormat][index]}
-            onChange={(e) => {
-              const newTexts = [...inputTexts[dayFormat]];
-              newTexts[index] = e.target.value;
-              setInputTexts(prevState => ({
-                ...prevState,
-                [dayFormat]: newTexts
-              }));
-            }}
-            style={{width: '100px', height: '10px'}}
-          /> 
-          <br/>
-        </div>
-      ))} 
-    </td>
+          {day.format('DD')}
+          {buttonVisible[dayFormat] && <button onClick={handleClick}>+</button>}
+          <br />
+          {/* input 박스의 onChange 이벤트를 처리하여 입력된 텍스트를 state에 저장합니다. */}
+          {(inputBoxes[dayFormat] || []).map((_, index) => (
+            <div>
+              <input
+                key={index}
+                type="text"
+                value={inputTexts[dayFormat][index]}
+                onChange={(e) => {
+                  const newTexts = [...inputTexts[dayFormat]];
+                  newTexts[index] = e.target.value;
+                  setInputTexts(prevState => ({
+                    ...prevState,
+                    [dayFormat]: newTexts
+                  }));
+                }}
+                style={{ width: '100px', height: '10px' }}
+              />
+              <br />
+            </div>
+          ))}
+        </td>
       );
     }
 
     // tr태그에 담을 코드 생성
     const weekDatesTr = (
-      // weekStart.format('YYYY-MM-DD')는 단순히 임시로 설정한 key값이다. 변경되어도 상관 없음
       <tr key={weekStart.format('YYYY-MM-DD')} >
-        {/* map으로 하게 되면 객체로,배열로 생성하기 때문에 우리가 생각하는 모양이 나 오지 않는다. */}
-        {/* 이미 weekDates로 배열로 생성했기 때문에 2차원 배열 개념으로 weekDatesRow에 삽입할 태그로 구상 가공만 하면 된다. */}
-        {/* {weekDates.map((date, index) => (<td key={index}>{date}</td>))} */}
         {weekDates}
       </tr>
     );
@@ -126,6 +143,7 @@ function App() {
           profile
         </div>
       </header>
+      {/* 컴포넌트 생성 */}
       <h1>{month}{nameMonth}</h1>
       <div className='container'>
         {/* 월별 버튼 */}
@@ -139,16 +157,12 @@ function App() {
 
         {/* 해당 날짜 달력 보여주기 */}
         <div className='month_view'>
-
           <div className='Calendar'>
             <table border='1'>
               <thead>
                 {/* 해당 날짜 Header */}
                 <tr>
                   {allDays.map((day, index) => (<td key={index}>{day}</td>))}
-                </tr>
-                <tr>
-                  {/* {dates.map((date, index) => (<td key={index}>{date}</td>))} */}
                 </tr>
               </thead>
               <tbody>
@@ -158,6 +172,7 @@ function App() {
           </div>
         </div>
       </div >
+      {/* 컴포넌트 생성 */}
     </>
   );
 }
